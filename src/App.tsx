caaa51d3ba929940c103096fa3fa54fd18cb7b5a
Julia <veluat@gmail.com>
2023-10-18 20:05:03 +0300

    feat: FlexWrapper added fix: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
 import './App.css';
 import styled from 'styled-components';
+import {Header} from "./layout/header/Header";
+import {Main} from "./layout/sections/main/Main";
 
 
 function App() {
     return (
-        <div className="App">
-            <Title>Styled-components <span>.attrs</span> method</Title>
-
-            <Form>
-                <Field/>
-                <Field/>
-                <Field/>
-            </Form>
-        </div>
+        <Wrapper className="App">
+            <Header/>
+            <Main/>
+        </Wrapper>
     );
 }
 
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    margin-bottom: 32px;
-    span {
-      color: #e91e63;
-    }
-`;
-
-const Form = styled.form`
-  width: 100%;
-  max-width: 500px;
-`;
-
-const Field = styled.input`
-  padding: 5px 15px;
-  margin: 10px 0;
-  width: 100%;
-  font-size: 1rem;
-`;
+const Wrapper = styled.div`
+background-color: #090F1D;
+  color: #FFFFFD;
+  font-size: 16px;
+`
---
+++ src/components/FlexWrapper.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+type FlexWrapperPropType = {
+    direction?: string
+    justify?: string
+    align?: string
+    wrap?: string
+    gap?: string
+}
+export const FlexWrapper = styled.div<FlexWrapperPropType>`
+  display: flex;
+  flex-direction: ${props => props.direction || "row"};
+  justify-content: ${props => props.justify || "flex-start"};
+  align-items: ${props => props.align || "stretch"};
+  flex-wrap: ${props => props.wrap || "nowrap"};
+`
