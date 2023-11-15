fb232986922265e70068eb48e52d0dea7dac9383
Julia <veluat@gmail.com>
2023-11-15 22:33:46 +0300

    style: Theme add

---
+++ src/style/Theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        primaryBg: 'rgba(9, 15, 29, 1)',
+        secondaryBg: 'rgba(19, 30, 58, 1)',
+        primary: 'rgba(211, 248, 90, 1)',
+        fontWhite: 'rgba(255, 255, 253, 1)',
+        fontGray: 'rgba(131, 131, 130, 1)'
+    },
+    padding: '92px 111px',
+}
