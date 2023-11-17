2e9fb47c173c9f710bac6dee0020a5fe2e2f780b
Julia <veluat@gmail.com>
2023-11-17 22:36:27 +0300

    style: Container add

---
+++ src/components/Container.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import {theme} from "../style/Theme";
+
+export const Container = styled.div`
+  max-width: 1512px;
+  width: 100%;
+  min-height: 100%;
+  margin: 0 auto;
+  padding: ${theme.padding};
+`
