4a06530202d165d9eb2fcc22b453f52403c0ea16
Julia <veluat@gmail.com>
2023-12-18 00:21:55 +0300

    style: adaptive and responsive style add

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {theme} from "../style/Theme";
 
 type FlexWrapperPropType = {
     direction?: string
@@ -12,6 +13,11 @@ export const FlexWrapper = styled.div<FlexWrapperPropType>`
   flex-direction: ${props => props.direction || "row"};
   justify-content: ${props => props.justify || "flex-start"};
   align-items: ${props => props.align || "stretch"};
-  flex-wrap: ${props => props.wrap || "nowrap"};
+  flex-wrap: ${props => props.wrap || "wrap"};
   gap: ${props => props.gap};
+
+  @media ${theme.media.tablet} {
+    justify-content: center;
+    align-items: center;
+  }
 `
