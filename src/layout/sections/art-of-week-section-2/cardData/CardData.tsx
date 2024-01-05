55c8c61fc34ee8d2f810fb02d8cf1755e28fa8e8
Julia <veluat@gmail.com>
2024-01-05 23:25:09 +0300

    feat: Local Data's added

---
+++ src/layout/sections/art-of-week-section-2/cardData/CardData.tsx
@@ -0,0 +1,30 @@
+import img1 from '../../../../assets/images/section-2-1.webp'
+import img2 from '../../../../assets/images/section-2-2.webp'
+import img3 from '../../../../assets/images/section-2-2.webp'
+
+const CardData: CardDataType[] = [
+    {
+        id: 1,
+        title: 'Cyberpunk Cocomo',
+        backgroundImage: img1,
+    },
+    {
+        id: 2,
+        title: 'Charge, Qi tiao yu',
+        backgroundImage: img2,
+    },
+    {
+        id: 3,
+        title: 'Surgeon, Josh Rife',
+        backgroundImage: img3,
+    },
+]
+
+// types
+    export type CardDataType = {
+        id: number
+        title: string
+        backgroundImage: string
+    }
+
+export default CardData;
---
+++ src/layout/sections/pop-artists-section-4/pop-data/PopData.ts
@@ -0,0 +1,66 @@
+import img1 from './../../../../assets/images/section-4-1.webp'
+import img2 from './../../../../assets/images/section-4-2.webp'
+import img3 from './../../../../assets/images/section-4-3.webp'
+import img4 from './../../../../assets/images/section-4-4.webp'
+import img5 from './../../../../assets/images/section-4-5.webp'
+import ava1 from './../../../../assets/images/ava-1.svg'
+import ava2 from './../../../../assets/images/ava-2.svg'
+import ava3 from './../../../../assets/images/ava-3.svg'
+import ava4 from './../../../../assets/images/ava-4.svg'
+import ava5 from './../../../../assets/images/ava-5.svg'
+
+
+const PopData: PopType[] = [
+    {
+        id: 1,
+        userName: 'Osvaldo Percy',
+        avatar: ava1,
+        bg: img1,
+        width: '300px',
+        height: '311px'
+    },
+    {
+        id: 2,
+        userName: 'Ranson Sqiure',
+        avatar: ava2,
+        bg: img2,
+        width: '520px',
+        height: '311px'
+    },
+    {
+        id: 3,
+        userName: 'Sebastian waltan',
+        avatar: ava3,
+        bg: img3,
+        width: '410px',
+        height: '311px'
+    },
+    {
+        id: 4,
+        userName: 'Abraham Zack',
+        avatar: ava4,
+        bg: img4,
+        width: '410px',
+        height: '311px'
+    },
+    {
+        id: 5,
+        userName: 'Cristio leo',
+        avatar: ava5,
+        bg: img5,
+        width: '410px',
+        height: '652px'
+    },
+]
+
+//types
+export type PopType = {
+    id: number
+    userName: string
+    avatar: string
+    bg: string
+    width: string
+    height: string
+}
+
+export default PopData;
