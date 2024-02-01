a5ce02976e3df73373cfa749863af1debd5e87e0
Julia <veluat@gmail.com>
2024-02-01 14:24:51 +0300

    style: responsive add

--- src/layout/sections/art-of-week-section-2/ArtOfWeekSection.styled.ts
+++ src/layout/sections/art-of-week-section-2/ArtOfWeekSection.styled.ts
@@ -2,15 +2,17 @@ import styled from "styled-components";
 
 export const CardContainer = styled.div`
   margin-top: 64px;
+  max-width: 1290px;
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
   gap: 30px;
 
   @media screen and (max-width: 1410px) {
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     gap: 16px;
   }
 `;
