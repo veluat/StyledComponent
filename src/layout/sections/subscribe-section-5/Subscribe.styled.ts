c8e18c5ff6d795739fb5bf3d932ce55ce2cc6ec5
Julia <veluat@gmail.com>
2024-02-03 18:02:37 +0300

    style: responsive add

--- src/layout/sections/subscribe-section-5/Subscribe.styled.ts
+++ src/layout/sections/subscribe-section-5/Subscribe.styled.ts
@@ -1,40 +1,15 @@
 import styled from "styled-components";
-import {theme} from "@assets/style/Theme.ts";
 
-export const ImgBox = styled.div`
-  max-width: 630px;
-  width: 100%;
-  height: 458px;
-  position: relative;
-`
+export const SubscribeWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 48px;
 
-export const FirstPhoto = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  transform: rotate(-12deg);
-`
-
-export const SecondPhoto = styled.div`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  z-index: 2;
-`
-
-export const ContentBox = styled.div`
-  max-width: 520px;
-  width: 100%;
-  height: 288px;
-`
-
-export const InputStyled = styled.input.attrs({title: 'text'})`
-  width: 516px;
-  min-height: 28px;
-  font-size: 16px;
-  border-radius: 8px;
-  border: 1px solid ${theme.colors.secondaryBg};
-  padding: 16px 24px;
-  background-color: ${theme.colors.secondaryBg};
+  @media screen and (max-width: 1410px) {
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    gap: 96px;
+  }
 `
--- src/layout/sections/subscribe-section-5/Subscribe.tsx
+++ src/layout/sections/subscribe-section-5/Subscribe.tsx
@@ -1,47 +1,15 @@
-import img from "@assets/images/subscribe/section-5-1.webp";
-import img1 from "@assets/images/subscribe/section-5-2.webp";
 import {Container} from "@components/container";
-import {FlexWrapper} from "@components/flex-wrapper";
-import {Photo} from "@components/photo";
-import {Title} from "@components/title";
-import {TextContent} from "@components/text-content";
-import {PrimaryButton} from "@components/primary-button";
-import {
-    ContentBox,
-    FirstPhoto,
-    ImgBox,
-    InputStyled,
-    SecondPhoto
-} from "@layout/sections/subscribe-section-5/Subscribe.styled.ts";
+import {SubscribePhotos} from "@layout/sections/subscribe-section-5/subscribePhotos/SubscribePhotos.tsx";
+import {SubscribeContent} from "@layout/sections/subscribe-section-5/subscribeContent/SubscribeContent.tsx";
+import {SubscribeWrapper} from "@layout/sections/subscribe-section-5/Subscribe.styled.ts";
 
 export const Subscribe = () => {
-    const title = 'Subscribe And '
-    const title2 = 'Every Week'
-    const primary = ['', 'get our Updates ']
     return (
-        <Container>
-            <FlexWrapper justify='space-between' align='center'>
-                <ImgBox>
-                    <FirstPhoto>
-                        <Photo img={img} width='410px' height='376px' radius='20px'/>
-                    </FirstPhoto>
-                    <SecondPhoto>
-                        <Photo img={img1} width='282px' height='348px' radius='16px'/>
-                    </SecondPhoto>
-                </ImgBox>
-                <ContentBox>
-                    <FlexWrapper direction='column' justify='flex-start' align='flex-start'>
-                        <Title title={title} primary={primary} title2={title2} mainTitle={false} width='520px'/>
-                        <TextContent
-                            text={`We have a blog related to NFT so we can share thoughts and routines on our blog which is updated weekly`}
-                            width='520px'/>
-                    </FlexWrapper>
-                    <FlexWrapper wrap='flex-wrap'>
-                        <InputStyled type="text" placeholder='Enter your e-mail'/>
-                        <PrimaryButton buttonName='Subscribe' width='160px'/>
-                    </FlexWrapper>
-                </ContentBox>
-            </FlexWrapper>
+        <Container id='subscribe'>
+            <SubscribeWrapper>
+                <SubscribePhotos/>
+                <SubscribeContent/>
+            </SubscribeWrapper>
         </Container>
     );
 };
