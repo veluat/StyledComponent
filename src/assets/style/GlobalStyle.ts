0ec5c5861cc3579ac8346d281d54459927ef81d7
Julia <veluat@gmail.com>
2024-02-18 12:16:33 +0300

    style: responsive add

--- src/assets/style/GlobalStyle.ts
+++ src/assets/style/GlobalStyle.ts
@@ -1,5 +1,7 @@
 import {createGlobalStyle} from "styled-components";
 import {theme} from "./Theme";
+import bg from "@assets/images/bg-image/BgImage.webp"
+import {font} from "@assets/style/Common.ts";
 
 export const GlobalStyle = createGlobalStyle`
   *,
@@ -18,6 +20,17 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     min-width: 360px;
+
+    overflow-x: hidden;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(${theme.colors.primaryBg} 0%,${
+            theme.colors.primaryBg
+    } 100%), url("${bg}");
+    background-blend-mode: multiply;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat
   }
 
   a {
@@ -35,4 +48,52 @@ export const GlobalStyle = createGlobalStyle`
     border: unset;
     cursor: pointer;
   }
+  
+  h1 {
+    ${font({
+    family: 'Canela',
+    weight: 500,
+    lineHeight: 1.2,
+    Fmax: 64,
+    Fmin: 39,
+})}
+  }
+
+  h2 {
+    ${font({
+    family: 'Canela',
+    weight: 500,
+    lineHeight: 1.2,
+    Fmax: 48,
+    Fmin: 31,
+})}
+  }
+
+  h3 {
+    ${font({
+    weight: 700,
+    lineHeight: 1.2,
+    Fmax: 20,
+    Fmin: 20,
+})}
+  }
+
+  h4 {
+    ${font({
+    weight: 700,
+    lineHeight: 1.2,
+    Fmax: 24,
+    Fmin: 20,
+})}
+  }
+
+  h5 {
+    ${font({
+    weight: 700,
+    lineHeight: 1.2,
+    Fmax: 16,
+    Fmin: 16,
+})}
+  }
+  
 `
