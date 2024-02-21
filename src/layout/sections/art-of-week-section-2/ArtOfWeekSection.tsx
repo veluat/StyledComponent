8df65b12c45ef2be83d163c7b24958efdad3bfaf
Julia <veluat@gmail.com>
2024-02-21 12:19:28 +0300

    style: responsive add

--- src/layout/sections/art-of-week-section-2/ArtOfWeekSection.tsx
+++ src/layout/sections/art-of-week-section-2/ArtOfWeekSection.tsx
@@ -2,29 +2,45 @@ import {FlexWrapper} from "@components/flex-wrapper";
 import {Container} from "@components/container";
 import {Title} from "@components/title";
 import {PrimaryButton} from "@components/primary-button";
-import {Card} from "@layout/sections/art-of-week-section-2/card";
-import cardData from "@layout/sections/art-of-week-section-2/cardData/CardData.tsx";
-import {CardContainer} from "@layout/sections/art-of-week-section-2/ArtOfWeekSection.styled.ts";
+import {Cards} from "@layout/sections/art-of-week-section-2/cards";
+import {useResponsiveSize} from "@/hook";
+import styled from "styled-components";
+import {theme} from "@assets/style/Theme.ts";
+import {CardData} from "@layout/sections/art-of-week-section-2/cardData";
 
 export const ArtOfWeekSection = () => {
-    const title = ' and Super Unique Art of This';
-    const primary = ['Amazing', ' Week'];
+    const title = ' and Super Unique Art of This'
+    const primary = ['Amazing', ' Week']
+    const isHide = useResponsiveSize(1410)
+    const isMobile = useResponsiveSize(theme.media.mobile)
+
     return (
         <Container id="amazing">
-            <FlexWrapper justify="space-between" align="flex-end">
-                <Title title={title} primary={primary} width="600px"/>
-                <PrimaryButton buttonName={'See All'}/>
-            </FlexWrapper>
-            <CardContainer>
-                <Card
-                    cardData={cardData}
-                    width="370px"
+            <FlexWrapper direction='column' gap={isHide ? '32px' : '64px'}>
+                <SectionHeader isHide={isHide}>
+                    <Title title={title} primary={primary} width={isMobile ? '357px' : '600px'}/>
+                    <PrimaryButton buttonName={'See All'} isHide={isHide}/>
+                </SectionHeader>
+
+                <Cards
+                    cardData={CardData}
+                    width='370px'
                     height="340px"
                     radius="16px"
-                    marginBottom="24px"
-                    widthButtonClear="178px"
+                    widthButtonClear={isMobile ? '168px' : '178px'}
                 />
-            </CardContainer>
+
+                {isHide &&
+                    <FlexWrapper justify='center'>
+                        <PrimaryButton buttonName={'See All'}/>
+                    </FlexWrapper>}
+            </FlexWrapper>
         </Container>
     );
 };
+const SectionHeader = styled.div<{ isHide: boolean }>`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: ${props => props.isHide ? 'center' : 'space-between'};
+  align-items: ${props => props.isHide ? 'center' : 'flex-end'};
+`
---
+++ src/layout/sections/art-of-week-section-2/cardsItem/index.ts
@@ -0,0 +1,1 @@
+export * from './CardsItem.tsx'
