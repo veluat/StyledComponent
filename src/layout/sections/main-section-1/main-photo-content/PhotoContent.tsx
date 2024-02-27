7380a5ce40b85b7b5560f1031098332e02c2413b
Julia <veluat@gmail.com>
2024-02-27 21:37:23 +0300

    style: responsive add

--- src/layout/sections/main-section-1/main-photo-content/PhotoContent.tsx
+++ src/layout/sections/main-section-1/main-photo-content/PhotoContent.tsx
@@ -5,19 +5,25 @@ import {Photo} from "@components/photo";
 import {CircleContent} from "@layout/sections/main-section-1/main-photo-content/circle-content";
 import {Promotion} from "@layout/sections/main-section-1/main-photo-content/promotion";
 import {PhotoContentStyled} from "@layout/sections/main-section-1/main-photo-content/PhotoContent.styled.ts";
+import {useResponsiveSize} from "@/hook";
+import {theme} from "@assets/style/Theme.ts";
 
 type PhotoContentPropsType = {
     mainData: MainType
 }
 export const PhotoContent: React.FC<PhotoContentPropsType> = ({mainData}) => {
+    const isMobile = useResponsiveSize(theme.media.mobile)
     return (
         <PhotoContentStyled>
             <FlexWrapper wrap="wrap">
-                <Photo img={mainData.backgroundImage} maxWidth='464px' maxHeight='544px' radius='24px'/>
+                <Photo img={mainData.backgroundImage}
+                       maxWidth={isMobile ? '294px' : '464px'}
+                       maxHeight={isMobile ? '344px' : '544px'}
+                       radius={isMobile ? '16px' : '24px'}/>
                 <CircleContent/>
+                <Promotion price={mainData.price} endingTime={mainData.endingTime}/>
             </FlexWrapper>
 
-            <Promotion price={mainData.price} endingTime={mainData.endingTime}/>
         </PhotoContentStyled>
     )
 }
