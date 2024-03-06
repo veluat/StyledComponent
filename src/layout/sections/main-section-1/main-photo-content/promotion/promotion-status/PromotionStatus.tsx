a93bc823ad4da5da064d96c4bf533fdf523b0d66
Julia <veluat@gmail.com>
2024-03-06 18:18:10 +0300

    feat: Promotion status add

---
+++ src/layout/sections/main-section-1/main-photo-content/promotion/promotion-status/PromotionStatus.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    PromotionStatusWrap
+} from "@layout/sections/main-section-1/main-photo-content/promotion/promotion-status/PromotionStatus.styled.ts";
+import {Span} from "@components/span";
+import {FlexWrapper} from "@components/flex-wrapper";
+import {theme} from "@assets/style/Theme.ts";
+
+export const PromotionStatus: React.FC<PromotionStatusPropsType> = ({title, value}) => {
+    return (
+        <PromotionStatusWrap>
+            <Span spanValue={title} color={theme.colors.primary} fontSize='14px' fontWeight='500'/>
+            <FlexWrapper align='center' gap='12px'>
+                <Span spanValue={value} fontSize='700' lineHeight='20px'/>
+            </FlexWrapper>
+        </PromotionStatusWrap>
+    )
+}
+
+type PromotionStatusPropsType = {
+    title: string
+    value: string
+}
