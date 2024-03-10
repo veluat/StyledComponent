62bae2f0a4b6cea1f14624b30450b4dea862e5fe
Julia <veluat@gmail.com>
2024-03-10 18:21:22 +0300

    feat: CardItem add

---
+++ src/layout/sections/art-of-week-section-2/cards/cards-item/CardItem.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {FlexWrapper} from "@components/flex-wrapper";
+import {Photo} from "@components/photo";
+import {Icon} from "@components/icon";
+import {
+    CardFullness,
+    CardImage,
+    CardItemStyled,
+    CardPrice,
+    CardTitle
+} from "@layout/sections/art-of-week-section-2/cards/cards-item/CardItem.styled.ts";
+import {Button} from "@components/button/Button.tsx";
+import {useResponsiveSize} from "@/hook";
+import {CardDataType} from "@layout/sections/art-of-week-section-2/card-data";
+import {
+    StatusCardBlock
+} from "@layout/sections/art-of-week-section-2/cards/cards-item/cards-status-block/StatusCardBlockProps.tsx";
+
+export const CardItem: React.FC<CardsItemProps & CardDataType> = ({
+                                                                      width,
+                                                                      height,
+                                                                      radius,
+                                                                      title,
+                                                                      price,
+                                                                      currency,
+                                                                      endingTime,
+                                                                      backgroundImage,
+                                                                      id
+}) => {
+    const isMobile = useResponsiveSize(576)
+    const isPoint = useResponsiveSize(620)
+
+    return (
+        <CardItemStyled>
+            <FlexWrapper direction="column" gap='24px'>
+                <CardImage>
+                    <Photo
+                        img={backgroundImage}
+                        maxWidth={width}
+                        maxHeight={id === 1 && isPoint ? '311px' : height}
+                        radius={radius}
+                    />
+                </CardImage>
+
+                <CardTitle>
+                    <FlexWrapper justify="space-between">
+                        <h3>{title}</h3>
+                        <CardPrice>
+                            <FlexWrapper align='center' gap='4px'>
+                                <Icon IconTitle='doubleArrow'/>
+                                <h5>{price}{currency}</h5>
+                            </FlexWrapper>
+                        </CardPrice>
+                    </FlexWrapper>
+                </CardTitle>
+
+                <CardFullness>
+                    <FlexWrapper justify="space-between">
+                        <StatusCardBlock title='Ending In' endingTime={endingTime}/>
+                        <Button btnType='outlined'
+                                buttonName='Place A Bid'
+                                width={isMobile ? '168px' : '178px'}/>
+                    </FlexWrapper>
+                </CardFullness>
+            </FlexWrapper>
+        </CardItemStyled>
+    )
+}
+
+type CardsItemProps = {
+    width: string
+    height: string
+    radius: string
+    id: number
+}
